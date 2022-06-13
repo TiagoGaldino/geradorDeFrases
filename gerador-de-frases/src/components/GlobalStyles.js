272f86e44ee960c3688b8e4a6499f2c5bb3109d1
Tiago Galdino <tiagogaldino@gmail.com>
2022-06-13 17:42:12 -0300

    criando aplicação

---
+++ gerador-de-frases/src/components/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        color: white;
+        padding: 0;
+        margin: 0;
+        font-family: serif;
+    }
+`;
